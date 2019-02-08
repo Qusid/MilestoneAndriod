d9b7cd9d87ee2079dfc2087d7431b9e3c350f34c
Qusid <44885165+Qusid@users.noreply.github.com>
2019-02-08 12:46:42 -0500

    Added Firebase + Upload Image feature now uploading (cloud V1)

--- app/src/main/java/com/example/milestoneandriod/UploadFeed.java
+++ app/src/main/java/com/example/milestoneandriod/UploadFeed.java
@@ -1,46 +1,43 @@
 package com.example.milestoneandriod;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+import android.app.AlertDialog;
+import android.widget.Toast;
 
+import java.lang.Object;
+import java.util.UUID;
 
 public class UploadFeed extends AppCompatActivity {
     // Image loading result to pass to startActivityForResult method.
     private static int SELECT_PICTURE = 1;
-
-    // GUI components
-    private Button button;  // The button
-    private ImageView image;// ImageView
-
-
+    android.net.Uri filePath;
+    //Firebase
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         FirebaseStorage storage = FirebaseStorage.getInstance();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_feed);
-
-        // Find references to the GUI objects
-        button = findViewById(R.id.btnChoose);
-        image =  findViewById(R.id.imagedisplay);
-
-
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,7 +47,7 @@ public class UploadFeed extends AppCompatActivity {
 
                 //Get ImageURi and load with help of picasso
                 //Uri selectedImageURI = data.getData();
-
+                filePath = data.getData();
                 Picasso.with(UploadFeed.this).load(data.getData()).noPlaceholder().centerCrop().fit()
                         .into((ImageView) findViewById(R.id.imagedisplay));
             }
@@ -64,6 +61,41 @@ public class UploadFeed extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
 
+    }
+
+    public void uploadImage(View v) {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(UploadFeed.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(UploadFeed.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 }
 
